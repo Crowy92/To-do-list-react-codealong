3919c17addb3bd9974f272c78170952ca51bcc6b
Tom Crowther <tom.crowther1992@gmail.comgit config --global user.name Crowy92>
2022-10-27 15:22:11 +0100

    finished

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -7,11 +7,21 @@ export default function TodoItem({todo, todos, setTodos}) {
             return todo.id !== el.id
         }))
     }
+    function handleComplete () {
+        setTodos( todos.map ((el) => {
+            if (todo.id === el.id){
+                return {
+                    ...el, completed: !todo.completed
+                }
+            }
+            return el
+        }))
+    }
   return (
     <div className='todo'>
-        <li key={todo.id} className="todo-item">{todo.text}</li>
-        <button className="complete-btn">done</button>
-        <button click={handleDelete} className="trash-btn">X</button>
+        <li key={todo.id} className={`todo-item ${todo.completed ? 'completed': ''}`}>{todo.text}</li>
+        <button onClick={handleComplete} className="complete-btn">done</button>
+        <button onClick={handleDelete} className="trash-btn">X</button>
     </div>
   )
 }
